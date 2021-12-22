4df84d198e125a27e68b32b9aa58981b4a9bb207
shouqiang.wei <shouqiang.wei@ichia.com>
2021-12-22 15:39:20 +0800

    公共头部

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-size: 12px;
 }
 *{
   margin: 0;
--- src/components/HeaderGuide.vue
+++ src/components/HeaderGuide.vue
@@ -3,29 +3,21 @@
     <div class="paten1"></div>
     <div class="paten2">
       <ul class="menu_list">
-        <li>
-          <a href="#">
-            公司信息
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            企业文化
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            业务体系
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            联系我们
+        <li v-for="menu in menus" :key="menu.index">
+          <a href="javascript:void(0)">
+            {{ menu.name }}
           </a>
         </li>
       </ul>
     </div>
-    <div class="paten3"></div>
+    <div class="paten3">
+      <ul class="menu_list">
+        <li v-for="item in mach" :key="item.index">
+          <img :src="item.src" alt="">
+          <span>{{ item.name }}</span>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
@@ -33,43 +25,88 @@
 export default {
   name: "HeaderGuide",
   data() {
-    return {};
+    return {
+      menus: [
+        { id: 1, name: "Home 网站首页" },
+        { id: 2, name: "Product 产品中心" },
+        { id: 3, name: "Service 技术服务" },
+        { id: 4, name: "Cases 样品展示" },
+        { id: 5, name: "About us 关于我们" },
+      ],
+      mach: [
+        { id: 1, name: "磁力研磨机", src: require("@/assets/imgae/磁力研磨机.png") },
+        { id: 2, name: "流动光饰机", src: require("@/assets/imgae/流动光饰机.png") },
+        { id: 1, name: "震动研磨机", src: require("@/assets/imgae/震动研磨机.png") },
+        { id: 1, name: "行星滚抛机", src: require("@/assets/imgae/行星滚抛机.png") },
+        { id: 1, name: "滚筒光饰机", src: require("@/assets/imgae/滚筒光饰机.png") },
+        { id: 1, name: "研磨块研磨液", src: require("@/assets/imgae/研磨块研磨液.png") }
+      ]
+    };
   },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .paten1 {
   background: rgba(48, 47, 47, 0.8);
-  height: 2rem;
+  height: 40px;
 }
 .paten2 {
   background: rgb(48, 47, 47);
-  height: 2rem;
-}
-.menu_list,
-.menu_list li,
-.menu_list li a {
-  width: auto;
-  display: block;
-}
-.menu_list {
-  margin-left: 50%;
-  transform: translateX(-50%);
-}
-.menu_list li {
-  float: left;
-}
-.menu_list li a {
-  height: 2rem;
-  padding: 0 1rem;
-  font-size: 14px;
-  line-height: 2rem;
-  color: white;
-  text-decoration: none;
+  height: 40px;
   text-align: center;
+  .menu_list,
+  .menu_list li,
+  .menu_list li a {
+    width: auto;
+    display: block;
+  }
+  .menu_list {
+    display: inline-block;
+    li {
+      float: left;
+      margin: 0 30px;
+      a {
+        height: 40px;
+        font-size: 12px;
+        line-height: 40px;
+        color: white;
+        text-decoration: none;
+        text-align: center;
+        &:hover {
+          color: red;
+        }
+      }
+    }
+  }
 }
-.menu_list li a:hover {
-  color: red;
+.paten3 {
+  background: #ccc;
+  height: 100px;
+  text-align: center;
+  .menu_list,
+  .menu_list li,
+  .menu_list li a {
+    width: auto;
+    display: block;
+  }
+  .menu_list {
+    display: inline-block;
+    li {
+      float: left;
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+      height: 100px;
+      align-items:center;
+      justify-content:center;
+      margin: 0 20px;
+      img {
+        width: 30px;
+        height: 30px;
+        margin: 0 auto;
+      }
+    }
+  }
 }
 </style>
